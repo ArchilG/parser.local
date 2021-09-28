4ae913201384b5f2416e09c38b29c0f28142a56f
Archil <archil-g@mail.ru>
2021-09-28 14:03:36 +0300

    GLX

--- app/Feeds/Vendors/GLX/Parser.php
+++ app/Feeds/Vendors/GLX/Parser.php
@@ -2,8 +2,8 @@
 
 namespace App\Feeds\Vendors\GLX;
 
-use App\Feeds\Feed\FeedItem;
 use App\Feeds\Parser\HtmlParser;
+use App\Feeds\Utils\ParserCrawler;
 use App\Helpers\StringHelper;
 
 class Parser extends HtmlParser
@@ -14,27 +14,25 @@ class Parser extends HtmlParser
     ];
 
     private array $images = [];
-    private string $name = '';
-    private string $description = '';
-    private array $shortDescription = [];
-    private string $sku = '';
-    private float $price = 0;
-    private float $listPrice = 0;
-    private int $available = 0;
-    private array $categories = [];
+    private float $weight = 0;
+    private array $short_description = [];
+    private array $attributes = [];
+
 
     public function beforeParse(): void
     {
         $offerText = $this->getText('[itemprop="offers"]');
         $matches = [];
         preg_match( '/Width:.+?(.*?)in/is', $offerText, $matches );
-        if (!empty($matches[1])) $this->dims['x'] = trim($matches[1]);
+        if (!empty($matches[1])) {
+            $this->dims['x'] = trim($matches[1]);
+        }
 
-        $matches = [];
         preg_match( '/Height:.+?(.*?)in/is', $offerText, $matches );
-        if (!empty($matches[1])) $this->dims['y'] = trim($matches[1]);
+        if (!empty($matches[1])) {
+            $this->dims['y'] = trim($matches[1]);
+        }
 
-        $matches = [];
         preg_match_all( "/preloadImage\('.*?,'\/\/(.*?)\?v/", $this->node->html(), $matches );
         if (!empty($matches[1])) {
             $this->images = array_map( static function ( $item ) {
@@ -42,131 +40,137 @@ class Parser extends HtmlParser
             }, $matches[ 1 ] );
         }
 
+        $weight = 0;
+        $short_description = [];
+        $attributes = [];
+        $next_is_descr = false;
+        $this->filter( '#viewitem-table tr' )->each( function ( ParserCrawler $item ) use ( &$weight, &$short_description, &$attributes, &$next_is_descr ) {
+
+            if(strstr($item->text(), 'Metal:')) {
+                $attributes['metal'] = StringHelper::removeSpaces(str_replace('Metal:','',$item->text()));
+            }
+
+            if(strstr($item->text(), 'Item Weight:')) {
+                $weight = StringHelper::removeSpaces(str_replace(['Item Weight:','gr.'],'',$item->text()));
+                $next_is_descr = true;
+            } else if($next_is_descr) {
+                $short_description[] = $item->text();
+            }
+
+
+        } );
+
+        $this->weight = $weight;
+        $this->short_description = $short_description;
+        $this->attributes = $attributes;
+
     }
     public function getProduct(): string
     {
-        $this->name = $this->getText('span[itemprop="name"]');
-        return  $this->name;
+        return  $this->getText('span[itemprop="name"]');
     }
 
     public function getImages(): array
     {
         if (!empty($this->images)) return $this->images;
-        $this->images = array_values(array_unique($this->getLinks('#altviews a')));
-        return $this->images;
+        return array_values(array_unique($this->getLinks('#altviews a')));
     }
 
     public function getDescription(): string
     {
-        $this->description = $this->getHtml('#product_description');
-        return  $this->description;
+        return  $this->getHtml('#product_description');
     }
 
     public function getShortDescription(): array
     {
-        $this->shortDescription = $this->getContent( '#viewitem-table .viewitemtable-data' );
-        return $this->shortDescription;
+        return $this->short_description;
     }
 
     public function getMpn(): string
     {
-        $this->sku = StringHelper::removeSpaces(str_replace('SKU: ', '', $this->getText('.product_code')));
-        return $this->sku;
+        return StringHelper::removeSpaces($this->getText('.product_code'));
     }
 
     public function getCostToUs(): float
     {
-        $this->price = $this->getMoney('[itemprop="price"]');
-        return $this->price;
+        return $this->getMoney('[itemprop="price"]');
     }
 
     public function getListPrice(): ?float
     {
-        $this->listPrice = $this->getMoney('.list-price');
-        return $this->listPrice;
+        return $this->getMoney('.list-price');
     }
 
     public function getAvail(): ?int
     {
-        $this->available = $this->getAttr('[itemprop="availability"]', 'content') === 'InStock' ? self::DEFAULT_AVAIL_NUMBER : 0;
-        return $this->available;
+        return $this->getAttr('[itemprop="availability"]', 'content') === 'InStock' ? self::DEFAULT_AVAIL_NUMBER : 0;
     }
     public function getCategories(): array
     {
-        $this->categories = $this->getContent('.vCSS_breadcrumb_td a');
-        array_shift($this->categories);
-        return array_filter($this->categories);
+        $categories = $this->getContent('.vCSS_breadcrumb_td a');
+        array_shift($categories);
+        return array_filter($categories);
     }
     public function getDimX(): ?float
     {
-        return $this->dims[ 'x' ];
+        return $this->dims[ 'x' ] ?? null;
     }
 
     public function getDimY(): ?float
     {
-        return $this->dims[ 'y' ];
+        return $this->dims[ 'y' ] ?? null;
     }
 
-    public function isGroup(): bool
+    public function getOptions(): array
     {
-        return $this->exists( '#options_table select[title="Gold Color"]' );
-    }
-    public function getChildProducts( FeedItem $parentFi ): array
-    {
-        $child = [];
-
-        $i = 0;
         $colors = $this->getContent( '#options_table select[title="Gold Color"] option' );
-        $ropeChains = $this->getContent( '#options_table select[title="Rope Chain Option"] option' );
+        $rope_chains = $this->getContent( '#options_table select[title="Rope Chain Option"] option' );
+        $box_chains = $this->getContent( '#options_table select[title="Box Chain Option"] option' );
+        $manufacurers_appraisals = $this->getContent( '#options_table select[title="Manufacturer\'s Appraisal"] option' );
+        $jewelry_tags = $this->getContent( '#options_table select[title="Jewelry Tag"] option' );
 
-        foreach ( $colors as $color ) {
-
-            $colorPrice = 0;
-            $ropeChainPrice = 0;
-            $matches = [];
-            preg_match( "/\[(.*?)\]/", $color, $matches );
-            if(!empty($matches[1])) {
-                $colorPrice = StringHelper::getMoney($matches[1]);
-                $colorText = trim(str_replace($matches[0],'',$color));
-            } else {
-                $colorText = trim($color);
-            }
-            if($ropeChains) {
-                foreach ( $ropeChains as $ropeChain ) {
-                    $i++;
-                    $ropeChainText = '';
-                    $ropeChainPrice = 0;
-                    $matches = [];
-                    preg_match( "/\[(.*?)\]/", $ropeChain, $matches );
-                    if(!empty($matches[1])) {
-                        $ropeChainPrice = StringHelper::getMoney($matches[1]);
-                        $ropeChainText = trim(str_replace($matches[0],'',$ropeChain));
-                    } else {
-                        $ropeChainText = trim($ropeChain);
-                    }
-                    $addPrice = $colorPrice + $ropeChainPrice;
-                    $name = "Gold Color: $colorText. Rope Chain Option: $ropeChainText. $this->name";
-                    $child[] = $this->buildChildProduct($parentFi, $name, $addPrice,$i);
-                }
-            } else {
-                $i++;
-                $name = "Gold Color: $colorText. $this->name";
-                $addPrice = $colorPrice + $ropeChainPrice;
-                $child[] = $this->buildChildProduct($parentFi, $name, $addPrice,$i);
+        $options = [];
+        foreach ( $colors as $val ) {
+            preg_match( "/\[(.*?)\]/", $val, $matches );
+            if(empty($matches[1])) {
+                $options['colors'][] = $val;
             }
         }
-        return $child;
+        foreach ( $rope_chains as $val ) {
+            preg_match( "/\[(.*?)\]/", $val, $matches );
+            if(empty($matches[1])) {
+                $options['rope_chains'][] = $val;
+            }
+        }
+        foreach ( $box_chains as $val ) {
+            preg_match( "/\[(.*?)\]/", $val, $matches );
+            if(empty($matches[1])) {
+                $options['box_chains'][] = $val;
+            }
+        }
+        foreach ( $manufacurers_appraisals as $val ) {
+            preg_match( "/\[(.*?)\]/", $val, $matches );
+            if(empty($matches[1])) {
+                $options['manufacurers_appraisals'][] = $val;
+            }
+        }
+        foreach ( $jewelry_tags as $val ) {
+            preg_match( "/\[(.*?)\]/", $val, $matches );
+            if(empty($matches[1])) {
+                $options['jewelry_tags'][] = $val;
+            }
+        }
+        return $options;
     }
 
-    private function buildChildProduct($parentFi, $name, $addPrice,$i) {
-        $fi = clone $parentFi;
-        $fi->setMpn(  $this->sku . "_" . $i);
-        $fi->setProduct( $name );
-        $fi->setCostToUs( $this->price + $addPrice );
-        $fi->setListPrice( $this->listPrice + $addPrice );
-        $fi->setRAvail( $this->available );
-        $fi->setDimX( $this->dims[ 'x' ]);
-        $fi->setDimY( $this->dims[ 'y' ] );
-        return $fi;
+    public function getWeight(): ?float
+    {
+        return $this->weight ?? null;
     }
+
+    public function getAttributes(): ?array
+    {
+        return $this->attributes ?? null;
+    }
+
 }
--- app/Feeds/Vendors/GLX/Vendor.php
+++ app/Feeds/Vendors/GLX/Vendor.php
@@ -4,8 +4,7 @@ namespace App\Feeds\Vendors\GLX;
 
 use App\Feeds\Feed\FeedItem;
 use App\Feeds\Processor\SitemapHttpProcessor;
-use App\Feeds\Utils\Data;
-use App\Feeds\Utils\ParserCrawler;
+use App\Feeds\Utils\Link;
 
 class Vendor extends SitemapHttpProcessor
 {
@@ -14,21 +13,6 @@ class Vendor extends SitemapHttpProcessor
     protected const DELAY_S = 0.5;
     protected const REQUEST_TIMEOUT_S = 60;
 
-    public function parseContent(Data $data, array $params = []): array
-    {
-        $this->node = new ParserCrawler( $data->getData(), $params[ 'url' ] ?? '' );
-        $this->uri = $params[ 'url' ] ?? '';
-        $this->getMeta();
-
-        $item = new FeedItem( $this );
-        if($this->exists( '[itemtype="http://schema.org/Product"]' )) {
-            $mpn = $item->isGroup() ? md5( microtime() . mt_rand() ) : $item->mpn;
-
-            return [ $mpn => $item ];
-        } else {
-            return [];
-        }
-    }
 //    public function filterProductLinks( Link $link ): bool
 //    {
 //        return str_contains( $link->getUrl(), '/Gold-Necklace-Bar-Diamonds-p/2120.htm' );
